fcc5ebbc2ff7a19ad86c009271a876f71686626c
Michał Gonczerzewicz <michalgon4@gmail.com>
2024-06-23 22:58:50 +0200

    testing

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -25,3 +25,22 @@ window.addEventListener('scroll', () => {
 //         document.getElementById('sticky').style.display = 'block';
 //     }
 // });
+
+// Get all h1 elements within the hero div
+const heroHeadings = document.querySelectorAll('.hero h1');
+let currentIndex = 0;
+
+// Function to switch the active class to the next h1
+function switchText() {
+  // Remove the active class from the current h1
+  heroHeadings[currentIndex].classList.remove('active');
+  
+  // Increment the index to the next h1
+  currentIndex = (currentIndex + 1) % heroHeadings.length;
+  
+  // Add the active class to the new current h1
+  heroHeadings[currentIndex].classList.add('active');
+}
+
+// Set interval to switch text every 30 seconds (30000 milliseconds)
+setInterval(switchText, 10000);
