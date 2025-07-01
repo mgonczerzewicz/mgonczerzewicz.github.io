79f75f7be0ff53ad330d04a1978441b204881914
Michał Gonczerzewicz <michalgon4@gmail.com>
2025-07-01 22:02:34 +0200

    connect

--- strona micgo/micgo/js/utils/servicesItems.js
+++
@@ -1,8 +0,0 @@
-export function initializeServicesItems() {
-	document.querySelectorAll('.service-title').forEach(title => {
-		title.addEventListener('click', () => {
-			const item = title.closest('.service-item')
-			item.classList.toggle('open')
-		})
-	})
-}
